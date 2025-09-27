b6d0b787616e1f29003a4343e8054a4b315c20cf
Noorik-975 <noorik975pk@gmail.com>
2025-09-27 14:22:38 +0600

    rereeee

---
+++ check/main.js
@@ -0,0 +1,9 @@
+
+
+const block = document.querySelector('.block')
+
+console.log(block);
+
+block.addEventListener('click', () => {
+  block.classList.toggle('block2')
+})
---
+++ site/had.js
@@ -0,0 +1,8 @@
+fetch('header.html')
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById('header').innerHTML = data;
+  })
+  .catch(error => console.error('Ошибка загрузки header:', error));
+
+  
